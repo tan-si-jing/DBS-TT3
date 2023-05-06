249b3e5843f363b0dc37ef0aa0184e3c6e416a2f
tan-si-jing <tansijing@yahoo.com.sg>
2023-05-06 13:36:12 +0800

    update query server controllers

--- server/query/controllers/transactionController.js
+++ server/query/controllers/transactionController.js
@@ -4,10 +4,10 @@ const Claims = db.claim;
 module.exports = {
   async create(req, res) {
     try {
-      const claims = await Claims.findAll({
-        attributes: ['CurrencyID'],
+      const claim = await Claims.findAll({
+        attributes: ["CurrencyID"],
       });
-      res.status(200).send(claims);
+      res.status(200).send(claim);
     } catch (err) {
       res.status(500).send({
         error: err.message || "An error has occurred trying to retrieve data.",
@@ -16,10 +16,15 @@ module.exports = {
   },
   async edit(req, res) {
     try {
-      const claims = await Claims.findAll({
-        attributes: ['CurrencyID'],
+      const claimId = req.params.id
+      const updates = req.params
+      delete updates.id
+      const claim = await Claims.update(updates, {
+        where: {
+          claimID: claimId,
+        },
       });
-      res.status(200).send(claims);
+      res.status(200).send(claim);
     } catch (err) {
       res.status(500).send({
         error: err.message || "An error has occurred trying to retrieve data.",
@@ -28,13 +33,15 @@ module.exports = {
   },
   async cancel(req, res) {
     try {
-      const claims = await Claims.findAll({
-        attributes: ['CurrencyID'],
+      await Claims.destroy({
+        where: {
+          claimID: req.params.id,
+        },
       });
-      res.status(200).send(claims);
+      res.status(200);
     } catch (err) {
       res.status(500).send({
-        error: err.message || "An error has occurred trying to retrieve data.",
+        error: err.message || "An error has occurred trying to cancel claim.",
       });
     }
   },
--- server/query/routes.js
+++ server/query/routes.js
@@ -4,33 +4,35 @@ const dashboardController = require("./controllers/dashboardController");
 const transactionController = require("./controllers/transactionController");
 const currencyController = require("./controllers/currencyController");
 
-module.exports = function(app) {
-    app.get("/api/dashboard", isLoggedIn, dashboardController.index);
-    app.post("/api/claim", isLoggedIn, transactionController.create);
-    app.put("/api/claim/:id", isLoggedIn, transactionController.edit);
-    app.delete("/api/claim/:id", isLoggedIn, transactionController.cancel);
-    app.get('/api/currencies', isLoggedIn, currencyController.index)
+module.exports = function (app) {
+  app.get("/api/dashboard", isLoggedIn, dashboardController.index);
+  app.post("/api/claim", isLoggedIn, transactionController.create);
+  app.put("/api/claim/:id", isLoggedIn, transactionController.edit);
+  app.delete("/api/claim/:id", isLoggedIn, transactionController.cancel);
+  app.get("/api/currencies", isLoggedIn, currencyController.index);
 
-    function isLoggedIn (req, res, next) {
-        let token = req.headers["x-access-token"];
-    
-        if (!token) {
-          return res.status(401).send({ message: "Login Required." });
+  function isLoggedIn(req, res, next) {
+    let token = req.headers["x-access-token"];
+
+    if (!token) {
+      return res.status(401).send({ message: "Login Required." });
+    }
+
+    verify(token, jwtSecret, (err, decoded) => {
+      if (err) {
+        if (err instanceof TokenExpiredError) {
+          return res
+            .status(401)
+            .send({ message: "Token has expired. Please log in again." });
         }
-    
-        verify(token, jwtSecret, (err, decoded) => {
-          if (err) {
-            if (err instanceof TokenExpiredError) {
-                return res
-                    .status(401)
-                    .send({ message: "Token has expired. Please log in again."});  
-            }
-            return res
-                .status(401)
-                .send({ message: "An error occured. Please try again. Error: " + err });  
-          }
-          req.body.params.id = decoded.id;
-          next();
-        });
-      };
-}
+        return res
+          .status(401)
+          .send({
+            message: "An error occured. Please try again. Error: " + err,
+          });
+      }
+      req.body.params.id = decoded.id;
+      next();
+    });
+  }
+};
