42393a4c553d4e8895b58a29e788cb0bf4d8df89
tan-si-jing <tansijing@yahoo.com.sg>
2023-05-06 11:00:50 +0800

    add server query routes

--- server/query/routes.js
+++ server/query/routes.js
@@ -1,3 +1,10 @@
+const dashboardController = require("./controllers/dashboardController");
+const transactionController = require("./controllers/claimController");
+
 module.exports = function(app) {
-    //
+    app.get("/api/dashboard", isLoggedIn, dashboardController.index);
+    app.post("/api/claim", isLoggedIn, transactionController.create);
+    app.put("/api/claim/:id", isLoggedIn, transactionController.edit);
+    app.delete("/api/claim/:id", isLoggedIn, transactionController.cancel);
+    app.get('/api/currencies', isLoggedIn, currencyController)
 }
