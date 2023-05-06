5854f237f818678dc6060bb0870cfbb1e76650f8
tan-si-jing <tansijing@yahoo.com.sg>
2023-05-06 13:56:03 +0800

    add database foreign key

--- server/query/models/index.js
+++ server/query/models/index.js
@@ -29,34 +29,56 @@ db.dept = require('./Department')(sequelize, Sequelize)
 // each project has many employees, one currency, many claims
 db.project.belongsToMany(db.employee, {
   through: 'EmployeeProjects',
-  timestamps: false
+  timestamps: false,
+  foreignKey: {
+    name: 'ProjectID',
+    allowNull: false
+  }
 });
 db.project.hasMany(db.claim, {
+  foreignKey: 'ProjectID',
   onDelete: 'CASCADE'
 });
 
 // each currency has many claims
-db.currency.hasMany(db.claim);
+db.currency.hasMany(db.claim,{
+  foreignKey: 'CurrencyID'
+});
 
 // each employee has one supervisor, one department, many projects and many claims
 db.employee.belongsTo(db.employee, {
-  foreignKey: "supervisor_id"
+  foreignKey: "SupervisorID"
 });
-db.employee.belongsTo(db.dept);
+db.employee.belongsTo(db.dept, {
+  foreignKey: "DepartmentCode"
+});
 db.employee.belongsToMany(db.project, {
   through: 'EmployeeProjects',
-  timestamps: false
+  timestamps: false,
+  foreignKey: {
+    name: 'EmployeeID',
+    allowNull: false
+  }
 });
 db.employee.hasMany(db.claim, {
+  foreignKey: 'EmployeeID',
   onDelete: 'CASCADE'
 });
 
 // each claim has one project and one employee
-db.claim.belongsTo(db.project);
-db.claim.belongsTo(db.employee);
-db.claim.belongsTo(db.currency);
+db.claim.belongsTo(db.project,{
+  foreignKey: 'ProjectID'
+});
+db.claim.belongsTo(db.employee,{
+  foreignKey: 'EmployeeID'
+});
+db.claim.belongsTo(db.currency,{
+  foreignKey: 'CurrencyID'
+});
 
 // each dept has many employees
-db.dept.hasMany(db.employee)
+db.dept.hasMany(db.employee,{
+  foreignKey: 'DepartmentCode'
+})
 
 module.exports = db
