b2da5385394c5900991de0bae4ad051982bfc225
wayne <flyingsloth@gmail.com>
2023-05-06 13:35:40 +0800

    add script to populate employee

--- server/populate.sql
+++ server/populate.sql
@@ -62,7 +62,6 @@ CREATE TABLE Currency (
 
 CREATE TABLE ProjectExpenseClaims (
     ClaimID int NOT NULL,
-    FollowUpID int,
     ProjectID int NOT NULL,
     EmployeeID int NOT NULL,
     CurrencyID varchar(50) NOT NULL,
@@ -78,7 +77,6 @@ CREATE TABLE ProjectExpenseClaims (
     FOREIGN KEY (ProjectID) REFERENCES Projects(ProjectID),
     FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID),
     FOREIGN KEY (CurrencyID) REFERENCES Currency(CurrencyID),
-    FOREIGN KEY (FollowUpID) REFERENCES ProjectExpenseClaims(ClaimID),
 
     CONSTRAINT CheckClaimStatus CHECK (Status IN ('pending', 'approved', 'rejected')),
     CONSTRAINT CheckEditedClaimDate CHECK (ExpenseDate <= LastEditedClaimDate)
@@ -90,22 +88,99 @@ INSERT INTO Department (DepartmentCode, DepartmentName)
 VALUES ("102", "Marketing");
 
 INSERT INTO Department (DepartmentCode, DepartmentName)
-VALUES ("103", "Cleaning");
+VALUES ("101", "Sales");
+
+INSERT INTO Department (DepartmentCode, DepartmentName)
+VALUES ("103", "Human Resources");
+
+INSERT INTO Department (DepartmentCode, DepartmentName)
+VALUES ("104", "Finance");
+
+INSERT INTO Department (DepartmentCode, DepartmentName)
+VALUES ("105", "Information Technology");
+
+INSERT INTO Department (DepartmentCode, DepartmentName)
+VALUES ("106", "Customer Service");
+
+INSERT INTO Department (DepartmentCode, DepartmentName)
+VALUES ("107", "Engineering");
+
+INSERT INTO Department (DepartmentCode, DepartmentName)
+VALUES ("108", "Research and Development");
+
+INSERT INTO Department (DepartmentCode, DepartmentName)
+VALUES ("109", "Procurement");
+
+INSERT INTO Department (DepartmentCode, DepartmentName)
+VALUES ("110", "Insurance");
 
 INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
-VALUES (10003, null, "102", "DBSBestBank2022", "Jerry", "Lim", "266177999");
-
+VALUES (10001, NULL, "101", "Singa@123", "Aisha", "Tan", "1234567890");
 INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
-VALUES (10001, 10003, "102", "DBSBestBank2022", "Tom", "Lim", "266177888");
+VALUES (10004, NULL, "102", "GulaMelaka#1", "Siti", "Baiduri", "4567890123");
+INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
+VALUES (10009, NULL, "105", "ShiokLah88#", "Wei Ting", "Tan", "9012345678");
+INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
+VALUES (10010, NULL, "105", "Majulah_Sg99", "Johnny", "Ng", "1123456789");
+INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
+VALUES (10017, NULL, "109", "Majulah_Sg77", "Rahman", "Tan", "8901234566");
+INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
+VALUES (10018, NULL, "109", "SedapNyumyum66", "Ying Ying", "Loh", "9012345677");
+INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
+VALUES (10002, 10001, "101", "LaKopi123!", "Jia Hui", "Lee", "2345678901");
+INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
+VALUES (10003, 10001, "102", "SedapNyumyum77", "Nurul", "Lim", "3456789012");
+INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
+VALUES (10005, 10002, "103", "Majulah*Sg123", "Shan", "Chong", "5678901234");
+INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
+VALUES (10006, 10002, "103", "ShiokLah99!", "Kai Ming", "Tan", "6789012345");
+INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
+VALUES (10007, 10003, "104", "ChopeSeat_88", "Rajesh", "Kumar", "7890123456");
+INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
+VALUES (10008, 10003, "104", "SiaLah#123", "Siti", "Jamilah", "8901234567");
+INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
+VALUES (10011, 10006, "106", "SedapNyumyum55", "Siti", "Zubaidah", "2345678903");
+INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
+VALUES (10012, 10006, "106", "KayaToast789#", "Kok Wai", "Lee", "3456789014");
+INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
+VALUES (10013, 10007, "107", "LaKopi_123", "Deepa", "Padukone", "4567890125");
+INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
+VALUES (10015, 10008, "108", "ChopeSeat99#", "Saravanan", "Kumar", "6789012349");
+INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
+VALUES (10016, 10008, "108", "GulaMelaka11#", "Xiao Mei", "Goh", "7890123450");
+INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
+VALUES (10019, 10011, "110", "LaKopi11#", "Munirah", "Yasin", "0123456781");
+INSERT INTO Employee (EmployeeID, SupervisorID, DepartmentCode, Password, FirstName, LastName, BankAccountNumber)
+VALUES (10020, 10011, "110", "SiaLah33#", "Ganesan", "Laksamana", "1234567892");
 
 INSERT INTO Projects (ProjectID, ProjectName, ProjectStatus, ProjectBudget, ProjectLeadID)
 VALUES (10005, "Project Name", "in progress", 0.99, 10003);
+
+
 
 INSERT INTO EmployeeProjects (ProjectID, EmployeeID)
 VALUES (10005, 10001);
 
 INSERT INTO Currency (CurrencyID, ExchangeRate)
-VALUES ("MYR", 3.30);
+VALUES ("CNY", 0.206);
+INSERT INTO Currency (CurrencyID, ExchangeRate)
+VALUES ("HKD", 0.17);
+INSERT INTO Currency (CurrencyID, ExchangeRate)
+VALUES ("IDR", 0.000093);
+INSERT INTO Currency (CurrencyID, ExchangeRate)
+VALUES ("INR", 0.018);
+INSERT INTO Currency (CurrencyID, ExchangeRate)
+VALUES ("JPY", 0.012);
+INSERT INTO Currency (CurrencyID, ExchangeRate)
+VALUES ("KHR", 0.00025);
+INSERT INTO Currency (CurrencyID, ExchangeRate)
+VALUES ("KRW", 0.0011);
+INSERT INTO Currency (CurrencyID, ExchangeRate)
+VALUES ("SGD", 1);
+INSERT INTO Currency (CurrencyID, ExchangeRate)
+VALUES ("TWD", 0.045);
+INSERT INTO Currency (CurrencyID, ExchangeRate)
+VALUES ("VND", 0.000044);
 
 INSERT INTO ProjectExpenseClaims (ClaimID, ProjectID, EmployeeID, CurrencyID, ExpenseDate, Amount, Purpose, ChargeToDefaultDept, AlternativeDeptCode, Status, LastEditedClaimDate)
 VALUES (11147, 10005, 10001, "MYR", '2023-11-11', 20000.0, "Venue Procurement", true, "103", "pending", null)
